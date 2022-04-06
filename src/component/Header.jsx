a120bdb1dcdb43b431030e7dfbd8f5bbacedbcb9
Raju Sahu <100789131+rajusahu89@users.noreply.github.com>
2022-04-06 17:12:05 +0530

    paymentform

---
+++ src/component/Header.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import './Style/header.css'
+
+const Header = () => {
+  return (
+    <div>
+        <div className="box1">
+    <img className="image" src= "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRlC8Ce263q6wB-ygngoAv4o0f42mL0KGNH9aRIXBLZbjXwQFvT1sFATkdgMOx7spZttZY&usqp=CAU" alt="" />
+    <h2 className='sign'>Sign Out</h2>
+  
+</div>
+    </div>
+  )
+}
+
+export default Header
